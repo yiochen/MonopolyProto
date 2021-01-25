055044cb666f9adcf9c367ba5e1da81691ade383
Yiou Chen <chen.yiou1992@gmail.com>
2021-01-24 18:40:11 -0800

    moved to Unity project

--- .copy.sh
+++ .copy.sh
@@ -1,4 +1,4 @@
 #!/usr/local/bin/fish
 
-ditto ../prisel/protos/gen_csharp/Prisel ./Runtime/Prisel
-ditto ../prisel/protos/gen_csharp/Monopoly/ ./Runtime/Monopoly
+ditto ../prisel/protos/gen_csharp/Prisel ./Assets/Runtime/Prisel
+ditto ../prisel/protos/gen_csharp/Monopoly/ ./Assets/Runtime/Monopoly
